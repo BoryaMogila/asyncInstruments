aec4d846ece3a2d687ff423203fe91a52322f726
boryam <borys.mohyla@ria.com>
2018-07-17 13:31:52 +0300

    examples

--- runners.js
+++ runners.js
@@ -1,21 +1,17 @@
 module.exports = [
     {
         name: 'Олег',
-        age: 18,
         time: 1000,
     },
     {
         name: 'Ігор',
-        age: 30,
         time: 3000,
     },
     {
         name: 'Борис',
-        age: 29,
         time: 2500,
     },
     {
         name: 'Василь',
-        age: 15,
     }
 ];
